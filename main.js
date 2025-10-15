dc2465b41947536789f30b5867c0c4c316d15200
giannivitale <giavitale78@gmail.com>
2025-10-15 15:51:46 +0200

    Modify and update html + js

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 console.log("it works");
 
+
+//Selezioniamo gli elementi
 const formEl = document.querySelector("form")
 
 const kmField = document.getElementById("km-field")
 const ageField = document.getElementById("age-field")
 const btnSubField = document.getElementById("btn-sub")
-const paxInfo = 
+const paxInfo = document.getElementById("result")
 
 
 console.log(kmField, ageField);
 
-formEl.addEventListener("submit",(event)=>{
-    event.preventDefault()
-    const info = `${kmField.value} ${ageField.value}`
+// Ascoltiamo con il "Submit"
 
-})
+formEl.addEventListener("submit",(event)=>{
+    event.preventDefault()// Cosí evitiamo che la pagina si ricarichi in auto
+    const km = kmField.value
+    const age = ageField.value
+
+    let price = kmField * (0.21)
+
+
+
+
+});
