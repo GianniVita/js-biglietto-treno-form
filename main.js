d4287a83fd6a18ba2ffbee3dfa08feb5f3e1aae4
giannivitale <giavitale78@gmail.com>
2025-10-15 15:13:35 +0200

    Add js const and prevent default for the form

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 console.log("it works");
 
+const formEl = document.querySelector("form")
 
-const 
+const kmField = document.getElementById("km-field")
+const ageField = document.getElementById("age-field")
+const btnSubField = document.getElementById("btn-sub")
+
+console.log(kmField, ageField);
+
+formEl.addEventListener("submit",(event)=>{
+    event.preventDefault()
+
+})
