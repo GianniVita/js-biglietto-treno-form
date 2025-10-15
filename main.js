e5b614ca1fadaedbd8e739a06ee3c4865332a877
giannivitale <giavitale78@gmail.com>
2025-10-15 15:29:38 +0200

    Stuck over js

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ const formEl = document.querySelector("form")
 const kmField = document.getElementById("km-field")
 const ageField = document.getElementById("age-field")
 const btnSubField = document.getElementById("btn-sub")
+const paxInfo = 
+
 
 console.log(kmField, ageField);
 
 formEl.addEventListener("submit",(event)=>{
     event.preventDefault()
+    const info = `${kmField.value} ${ageField.value}`
 
 })
